e18c5195f93a80cb9c21976307058be1f572449e
Andre F <DSFdesigns@users.noreply.github.com>
2023-03-26 11:34:38 +0900

    2023/03/26
    Updates to cafe

--- cafe/scripts/app.js
+++ cafe/scripts/app.js
@@ -4,7 +4,7 @@
 function initMap() {
   // The location of Uluru
   const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
+  // The map, centered at uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: uluru,
