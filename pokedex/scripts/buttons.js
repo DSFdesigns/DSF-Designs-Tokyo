19d8b1f0737975714ec5fb41ddba00876a5dacb0
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-05-11 09:22:29 +0900

    20260511
    Updates to pokedex

--- pokedex/scripts/buttons.js
+++ pokedex/scripts/buttons.js
@@ -1,29 +1,27 @@
-    const button = document.getElementById("saveButton");
-    // Load saved state
+// Button save state script
+	const button = document.getElementById("saveButton");
+// Load saved state
     let savedState = localStorage.getItem("buttonState");
     if(savedState === "on"){
         button.classList.remove("off");
         button.classList.add("on");
         button.textContent = "ON";
     }
-
-    // Toggle button state
+// Toggle button state
     button.addEventListener("click", () => {
 
         if(button.classList.contains("off")){
             button.classList.remove("off");
             button.classList.add("on");
             button.textContent = "ON";
-
-            // Save state
+// Save state
             localStorage.setItem("buttonState", "on");
 
         } else {
             button.classList.remove("on");
             button.classList.add("off");
             button.textContent = "OFF";
-
-            // Save state
+// Save state
             localStorage.setItem("buttonState", "off");
         }
     });
--- pokedex/scripts/date.js
+++ pokedex/scripts/date.js
@@ -1,3 +1,33 @@
 // Update Year //
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
+// Button save state script
+	const button = document.getElementById("seenButton");
+//	const button = document.getElementById("saveButton");
+// Load saved state
+    let savedState = localStorage.getItem("buttonState");
+    if(savedState === "on"){
+        button.classList.remove("off");
+        button.classList.add("on");
+        button.textContent = "ON";
+    }
+// Toggle button state
+    button.addEventListener("click", () => {
+
+        if(button.classList.contains("off")){
+            button.classList.remove("off");
+            button.classList.add("on");
+            button.textContent = "ON";
+// Save state
+            localStorage.setItem("buttonState", "on");
+
+        } else {
+            button.classList.remove("on");
+            button.classList.add("off");
+            button.textContent = "OFF";
+// Save state
+            localStorage.setItem("buttonState", "off");
+        }
+    });
+	
+	
